852946020edd479933dbf25dcd7a10fcb5cd3090
Andrew Gopienko <a.gopienko@ndmsystems.com>
2019-09-20 15:32:29 +0700

    feature(api): add resolver

---
+++ api/resolver/resolver.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"io"
+	"net"
+)
+
+// ResolverService is an interface for host name ipv4 resolver
+type ResolverService interface {
+	AddHost(host string)
+	DelHost(host string)
+	GetIP(host string) string
+	GetIPs(host string) ([]net.IP, []net.IP)
+	GetIPsStr(host string) ([]string, []string)
+	Dump(w io.Writer)
+}
